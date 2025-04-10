9e261800569412d9169d753c9cb76b88691ba693
DilanQuiros1 <quiroschinchilladilan@gmail.com>
2025-04-10 14:21:22 -0600

    Updated token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,18 @@
 const VerifyToken = (req, res) => {
-  res.send("Hola VerifyToken");
+  try {
+    var accessToken = "TOKIASJAPGHDSTWFASAERP10";
+    var token = req.query["hub.verify_token"];
+
+    var challenge = req.body["hub.challenge"];
+
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (e) {
+    res.status(400).send();
+  }
 };
 
 const ReceivedMessage = (req, res) => {
